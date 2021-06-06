2b2952ce2538789671abcbd11649a5e7f7494d4c
temirlan <ashimkhan.temirlan@gmail.com>
2021-06-07 01:27:34 +0600

    update scripts

--- sos_app/static/signin/js/main.js
+++ sos_app/static/signin/js/main.js
@@ -4,7 +4,8 @@ var x, i, j, selElmnt, a, b, c;
 /* Look for any elements with the class "custom-select": */
 x = document.getElementsByClassName("custom-select");
 for (i = 0; i < x.length; i++) {
-  selElmnt = x[i].getElementsByTagName("select")[0];
+  selElmnt = x[i].getElementsByClassName("card_region")[0];
+  console.log(selElmnt, x.length, i);
   /* For each element, create a new DIV that will act as the selected item: */
   a = document.createElement("DIV");
   a.setAttribute("class", "select-selected");
